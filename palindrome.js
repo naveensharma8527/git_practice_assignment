51de05b19c6c05e557e235c520269fff51234844
naveensharma8527 <nsnaveensharma1998@gmail.com>
2022-09-20 21:03:11 +0530

    wrong code

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+function isPalindrome(string){
+	let flag = true
+	for(let i=0;i<string.length;i++){
+		if(string[i] !== string[string.length-1]){
+			flag = false;
+			break;
+		}
+	}
+	if(flag){
+		return true;
+	}
+	return true;
+}
---
+++ prime.js
@@ -0,0 +1,12 @@
+function checkPrime(num){
+	let count = 0;
+	for(let i =1;i<=num;i++){
+		if(num/i == 0){
+			count++
+		}
+	}
+	if(count==2){
+		return true;
+	}
+	return false;
+}
